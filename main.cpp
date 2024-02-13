20a128f6ca15b21b83eb6cd351f86ea1756c161c
dxrkexist <32045730-dxrkexist@users.noreply.replit.com>
2024-02-13 19:32:55 +0000

    Mass

--- main.cpp
+++ main.cpp
@@ -1,5 +1,44 @@
 #include <iostream>
+#include <ctime> // Добавляем заголовочный файл для функции time()
+
+using namespace std;
 
 int main() {
-  std::cout << "Hello World!\n";
+    srand(time(NULL));
+
+    int mas1[6] = {5, 0, -2, 0, 17, 8};
+    cout << endl << endl;
+    cout << "Масив mas1: ";
+    cout << mas1[0] << ' ' <<
+        mas1[4] << ' ' << mas1[2];
+
+    int mas2[4];
+
+    mas2[0] = 100;
+    mas2[3] = 200;
+
+    cout << endl << endl;
+    cout << "Масив mas2: ";
+    cout << mas2[0] << ' ' <<
+        mas2[1] << ' ' << mas2[2] << ' ' << mas2[3];
+
+    cout << endl << endl;
+    char symbols[7] = {'W', 'h', '$', '@', 'W', '*', '!'};
+    cout << "Масив symbols: ";
+    for (int i = 0; i < 7; i++) {
+        cout << symbols[i] << ' ';
+    }
+
+    int mas3[10];
+    for (int i = 0; i < 10; i++) {
+        mas3[i] = rand() % 100;
+    }
+
+    cout << endl << endl;
+    cout << "Масив mas3: ";
+    for (int i = 0; i < 10; i++) {
+        cout << mas3[i] << ' ';
+    }
+
+    return 0;
 }
